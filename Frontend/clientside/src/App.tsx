eeb23aeb32397845e67001d5844febe9bead9041
ANKIT BOHARA <ankitbohara539@gmail.com>
2026-07-14 00:33:06 +0545

    chore(project): initialize project with basic structure

---
+++ Frontend/clientside/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Welcome to Scholar AI</h1>
+      <p>Your AI-Powered University and Scholarship Recommendation Platform</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
